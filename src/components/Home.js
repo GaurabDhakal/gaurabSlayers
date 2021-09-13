3f8c695a8be30dc2838d40a56f5f0ced69bdb1ea
GaurabDhakal <dhakalgaurab123@gmail.com>
2021-09-13 10:17:59 +0545

    init

---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+function Home() {
+    return (
+      <div>
+
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+
+    {/* <h1>Maintainence going on be patience!!</h1> */}
+
+    <h3 align="center">Welcome to the Official Website of Team Slayers!!</h3>
+    <p align="center">#WeAreTheBest</p>
+    <br/>
+    <h6 align="center">
+Under Development
+    </h6>
+    <h6 align="center">1.TS BHOOT</h6>
+    <img src="https://i.im.ge/2021/09/12/Tsg6qX.jpg" className="rounded mx-auto d-block" alt="Bhoot" /><br />
+    <h6 align="center">2.TS SAKKU</h6>
+    <img src="https://i.im.ge/2021/09/12/TsgNP8.jpg" className="rounded mx-auto d-block" alt="Sakku" /><br />
+    <h6 align="center">3.TS DINESH</h6>
+<img src="https://i.im.ge/2021/09/12/TsgPkh.jpg" className="rounded mx-auto d-block" alt="Dinesh" />
+
+</div>
+        )
+}
+
+export default Home
---
+++ src/components/contact.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import emailjs from 'emailjs-com';
+
+function contact(props) {
+ const send= function send(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('Gmail', 'template_zf6gy35', e.target, 'user_L2T1HCmWmP1yXeNfXwkL2')
+      .then((result) => {
+         alert("Email Sent You will be mailed after review," +result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  }
+
+    return (
+        <div className="container">
+          <h3 className="mb-20" >This Form will Not Work For Now!!</h3>
+          <form onSubmit={send}>
+        <div className="mb-3">
+  <label htmlFor="exampleFormControlInput1" className="form-label">Email address:</label>
+  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="email"/>
+</div><br /><div className="mb-6">
+<label htmlFor="exampleFormControlInput1" className="form-label">UID:</label>
+<input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Eg:577247729" name="uid"/>
+  <br/>
+  <label htmlFor="exampleDataList" className="form-label">What's Your IG Role?</label>
+<input className="form-control" name="role" list="datalistOptions" id="exampleDataList" placeholder="What's Your IG Role?" />
+<datalist id="datalistOptions">
+  <option value="Sniper" />
+  <option value="Rusher" />
+  <option value="Flanker" />
+  <option value="Leader" />
+  <option value="Other!" />
+</datalist></div>
+<div className="mb-26">
+  <label htmlFor="exampleFormControlTextarea1" className="form-label">Why do you want to Join?</label>
+  <textarea name="reason" className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div><br />
+<input type="submit" value="Submit" className="btn btn-outline-primary"/>
+</form>
+        </div>
+    )
+}
+
+
+export default contact
+
